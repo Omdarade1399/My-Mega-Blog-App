a117d7d5f99003a0dd67be730ef96ebd7a6b3af8
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-10-24 21:37:59 +0530

    building services for database posts, file uploads and queries complete

--- src/Appwrite/configure.js
+++ src/Appwrite/configure.js
@@ -13,6 +13,112 @@ export class Service {
         this.databases = new Databases(this.client)
         this.bucket = new Storage(this.client)
     }
+
+    async createPost({title, slug, content, featuredImage, status, userId}) {
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status, 
+                    userId
+                }
+            )
+        } catch (error) {
+            return error
+        }
+    }
+
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            return error
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true
+        } catch (error) {
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            return error
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            return error
+        }
+    }
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            return error
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                fileId
+            )
+            return true
+        } catch (error) {
+            return error
+        }
+    }
+
+    getFilePreview(fileId){
+        return  this.bucket.getFilePreview(
+            config.appwriteBucketId,
+            fileId
+        )
+    }
 }
 
 const service = new Service()
